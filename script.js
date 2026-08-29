041e79ebbec1b240001490dd23f83010ea2b0027
thecasualgoldfish <gomezvic102@gmail.com>
2026-08-29 15:56:38 -0400

    hello

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function savevals() {
+  const inputs = document.querySelectorAll("input");
 
+  const dict = {};
+  inputs.forEach(input => {
+    if (input.id) {
+      dict[input.id] = input.value;
+    }
+  });
+
+  localStorage.setItem("allInputs", JSON.stringify(dict));
+  console.log(dict);
+  return dict;
+}
