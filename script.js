f79edd87537c3b5a40c145ebc99121f5185ce462
thecasualgoldfish <gomezvic102@gmail.com>
2026-08-29 14:07:35 -0400

    functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function submitPrompt()
+function getTask() 
 {
-    
+  const input = document.getElementById("usersTask");
+  taskbet = input.value; // save for later
+  localStorage.setItem("savedInput", taskbet); // optional: keeps it after refresh
+  return taskbet;
+}
+
+function getAmount() {
+  const input = document.getElementById("usersTask");
+  moneybet = input.value; // save for later
+  localStorage.setItem("savedInput", moneybet); // optional: keeps it after refresh
+  return moneybet;
 }
